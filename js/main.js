cc4be42d460a8a9371d5ce9e72e93ad21ca05ec2
Vitorleony <vleony953@gmail.com>
2022-11-10 15:53:05 -0300

    tcc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,196 @@
-window.sr = ScrollReveal({reset: true});
+var games = [
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.50759.69802328231064156.03ff84cc-b6b1-4226-8e6c-545d183e5fbf.6c46b7d8-06fd-4959-8342-440580385ce9?w=412&format=jpg',
+        title: '7 Days to Die',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.55802.13714290489749263.fa316741-1615-4cad-942c-7c2fddc7b069.86d4a18f-cf41-4fa9-92af-8c95ca311c96?w=412&format=jpg',
+        title: 'A Memoir Blue',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.60562.13558336166432541.beb57fbe-cc4b-40c5-ba76-c8112867dea2.9a69e497-4f9f-495d-9ac6-f8956b491d91?w=412&format=jpg',
+        title: 'A Plague Tale: Requiem',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.63573.14277836659846754.5f133eeb-446d-45d9-bc2f-294eea5a641c.c71fe1f8-f8e8-4c71-bffb-e1bc9f37ae77?w=412&format=jpg',
+        title: 'ANVIL : Vault Breaker (Game Preview)',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.14842.14036931078975578.35527dd4-bd1c-43d9-90ff-3a801a47df6a.e296f313-9109-4635-a3cb-14b747fb2230?w=412&format=jpg',
+        title: 'ARK: Ultimate Survivor Edition',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.64849.13510798887933723.57e43f19-4066-429e-b1a2-caea56e427b4.860c35a4-ccbf-413e-9574-1c7c8826d6c6?w=412&format=jpg',
+        title: 'ASTRONEER',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.59370.14189206062547849.34667497-3c97-48d3-916d-0cd555e3aee9.2a32a391-5b37-4ec9-9fc1-7bb4159de6b9?w=412&format=jpg',
+        title: 'Aliens: Fireteam Elite',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.38348.70644597693904836.cde9dd08-509f-4054-8190-24728e59ad5e.3d59232d-5678-4cf5-abaf-d4371ea14d51?w=412&format=jpg',
+        title: 'Amnesia: Collection',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.43953.14290925569393643.f558cfd6-0d70-43e7-aaba-ddbe80b67784.48c00b8a-ec33-43e2-a4d2-083e95994d98?w=412&format=jpg',
+        title: 'Amnesia: Rebirth',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.21162.13589262686196899.16e3418a-cbf2-4748-9724-1c9dc9b7a0b9.14afafe9-1b03-4df0-95ad-12e2712a3b53?w=412&format=jpg',
+        title: 'Among Us',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.22986.14347616248294866.f2c12ab4-ed34-4e28-b51d-b89260f65198.c2f48fe0-70e2-4a54-af39-02de867401ba?w=412&format=jpg',
+        title: 'Aragami 2',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.20086.14539916772838877.ac2b2d42-d84b-4c24-8fe1-11b90b3ed1f0.291df9fa-27ba-42d9-b74b-40c45a59f1b8?w=412&format=jpg',
+        title: 'Archvale',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.46746.14387733153613072.28665954-14dc-4229-8e3c-0257e25089cb.46b89faa-6b2f-433b-aefc-16e5063ea2af?w=412&format=jpg',
+        title: 'As Dusk Falls',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.45811.71972716530068101.91350a92-63c5-4583-96a7-c8e03d0d6c67.a5f46c28-e00c-40a7-b5e0-a723a407bb16?w=412&format=jpg',
+        title: "Assassin's Creed® Odyssey",
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.24317.63657969076271726.d61cc1bb-fe99-40ef-b061-bded231757be.c17f684c-0c90-4d39-911e-8bd8c0622587?w=412&format=jpg',
+        title: "Assassin's Creed® Origins",
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.1712.69676224553003885.7746d59f-9224-4b66-8097-e540a8ad4b00.a2e18719-7a03-42b0-9324-907f92092527?w=412&format=jpg',
+        title: 'BLiNX: The Time Sweeper',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.49309.63183727176145146.d84d29ef-df3a-4a5d-96b4-f1a7d9797aec.7b42c961-b9e0-484c-b079-f461f64e1f53?w=412&format=jpg',
+        title: 'Back 4 Blood',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.34826.71100151761088573.88824642-fc7e-4e99-9b5b-32e26f7c27b9.8a1cccf6-01d0-48c6-bd1a-0d03df399bba?w=412&format=jpg',
+        title: 'Banjo Kazooie: N n B',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.58897.70306648669442933.0ad02883-9547-43ed-a595-2a807684ce15.fa8d898f-1097-46fa-b35d-22ceefda2a7a?w=412&format=jpg',
+        title: 'Banjo-Kazooie',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.2699.63435052096425467.cd1eef72-5707-4f2b-807d-ab3c5baf858c.9ebaa8fc-f1bf-49e8-93fd-62833dbb6b26?w=412&format=jpg',
+        title: 'Banjo-Tooie',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.50680.69836087516172366.d802940c-fd8a-4174-8a68-e41a2475e1a1.efa1f648-fb98-4078-80cf-06f47811b1fa?w=412&format=jpg',
+        title: 'Batman™: Arkham Knight',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.18274.67660042887481018.7de2bfcd-48ef-4a6c-bba4-e65d208d67ec.f299bd07-f6b3-4870-b5e6-786c0a4c3e92?w=412&format=jpg',
+        title: 'Battlefield 4',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.46163.68596255437297164.99e6fb73-cd5e-440f-90c2-d8d2eb40a69d.5f3530f4-f0eb-4705-853c-3c5067bddad5?w=412&format=jpg',
+        title: 'Battlefield™ 1 Revolution',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.242.64032621253256243.9745cf01-8239-4973-8193-31de18936f03.a8c3c497-3428-45cb-85b3-d84dcb2743bd?w=412&format=jpg',
+        title: 'Battlefield™ V',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.15107.14065499269907016.249670e2-cad9-48bb-aa0f-309d712ede80.0cc02dad-9d94-4844-9ffe-63d0b9055b6f?w=412&format=jpg',
+        title: 'Battletoads',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.23560.14588206397851258.078192f2-519f-4a81-b68d-2dbd6199f2c2.af953250-54af-4a2a-be0c-163b68917421?w=412&format=jpg',
+        title: 'Beacon Pines',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.64240.13938260246595322.6dab1a29-4d96-4a37-b22b-2baaeba55633.f371254b-cef0-4600-ad24-a5c03f04615a?w=412&format=jpg',
+        title: 'Before We Leave',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.30849.13672727399820228.e1322d27-4961-46da-9c7b-c310d3e28cb1.90cdc8cc-0bdd-4b06-ba46-71549ba3bf46?w=412&format=jpg',
+        title: 'Ben 10: Uma super viagem',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.43009.14326017138836689.0723eba4-7b1a-4eb0-8e6a-5c58708793ed.d0c2a3f0-b262-45a4-ab27-833dd29fda31?w=412&format=jpg',
+        title: 'Besiege Console (Game Preview)',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.55168.70463400880857175.d5efc868-ff4c-4751-bfbd-5f5b12ceed46.f8533b63-65dc-4100-9db6-a2b328bf176a?w=412&format=jpg',
+        title: 'Black Desert',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.5290.14431087147384961.13dc1a18-3532-477b-b2d2-772256efc040.6fab8e78-8a3d-4b85-a5b1-33f32aeb8ea3?w=412&format=jpg',
+        title: 'Bleeding Edge',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.64781.14387940932825223.b2bcb764-5659-4e3e-be75-fea7de3ead94.36290a14-76b1-4cda-b2b6-1991f2e95476?w=412&format=jpg',
+        title: 'Breathedge',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.53942.68088896991488711.7d62a8af-1aa7-44bf-a425-105cabf5c97f.6aaec0f6-8abc-4ed7-a0dc-4ba047115e76?w=412&format=jpg',
+        title: 'Bridge Constructor Portal',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.11626.69320940561073110.189571f5-bf61-4250-851f-3d627dfcdc29.bed721b9-7f8d-4800-ad0b-7a07807c0863?w=412&format=jpg',
+        title: 'Broken Age',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.19620.14527663576451809.5be6f978-a9ed-42c5-8ee1-87dc4d947724.7e2ac7bd-0040-4c16-80e1-3a7cf1d566a7?w=412&format=jpg',
+        title: 'Bugsnax',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.32647.14407039492580836.0ee1fbde-13a0-4676-975c-913f27fd0ac6.5402bc40-0e5f-434f-8cc2-a738028aac3c?w=412&format=jpg',
+        title: 'Chinatown Detective Agency',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.61947.14071745200459129.81e3e86f-3ab4-4027-b9a1-81f595fcb505.fc2487e4-7785-4a73-b2ad-b94aa01a7e7f?w=412&format=jpg',
+        title: 'Chivalry 2',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.19672.13751787478651765.747d9b13-75e6-4c34-a196-72624dc19de6.c2d9f22b-a5f5-4802-997c-7a5b2780895d?w=412&format=jpg',
+        title: 'Chorus',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.42716.66617542682682743.811213f2-2c45-4145-973d-fe3e3774b196.c2dab488-7382-45d1-bf6c-6b8be6e3fd96?w=412&format=jpg',
+        title: 'Cities: Skylines - Xbox One Edition',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.55562.14019812399166283.86c56d8c-9235-45b0-85dd-dc9647631d6e.d1c98280-ac63-4f01-85ed-41e58c8fac35?w=412&format=jpg',
+        title: 'Citizen Sleeper',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.11196.63460736466173537.43accb13-0658-4cb9-a3a9-2dd4b18ae15e.c12756d4-a28f-46d2-89d0-43593731f0d5?w=412&format=jpg',
+        title: 'ClusterTruck',
+    },
+    {
+        image: 'https://store-images.s-microsoft.com/image/apps.30354.13910765112993759.85e2446b-e867-4d17-a642-a74c828870ce.c92e92ee-4550-424d-9389-a717815bd19f?w=412&format=jpg',
+        title: 'Coffee Talk',
+    },
+];
 
-sr.reveal('.navbar', {
-    rotate: {x: 0, y: 80, z: 0},
-    duration: 2000
+console.log(games);
+
+var parent = document.querySelector('#parent');
+parent.innerHTML = null;
+games.forEach((game) => {
+    parent.innerHTML += `
+    
+        <main id="parent">
+        
+             <div class="card">
+                    <img src="${game.image} " style="width: 100%">
+                    <h2>${game.title}</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Praesentium ipsum doloribus provident rerum,
+                        animi laborum dicta culpa magnam minus fugiat ipsam</p>
+                        <a href="#">Comprar</a>
+                    </div>
+                </div>
+          
+        </main>
+
+    `;
 });
+
+
