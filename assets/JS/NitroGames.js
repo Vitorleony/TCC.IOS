62bc96f861f2154c3c651ac3db9d5fa875acd9a0
Vitorleony <vleony953@gmail.com>
2022-11-30 23:23:01 -0300

    atulização13

--- assets/JS/NitroGames.js
+++ assets/JS/NitroGames.js
@@ -1,25 +1,40 @@
 // 
-var mostraCardJogo = document.querySelector('#mostraCardJogo')
+// var mostraCardJogo = document.querySelector('#mostraCardJogo')
 
-var pesquisaJogos = document.querySelector('.pesquisaJogos')
+// var pesquisaJogos = document.querySelector('.pesquisaJogos')
 
-const  chamarJogo = () => {
+// const  chamarJogo = () => {
 
-    var pegaValor =  pesquisaJogos.value;
+//     var pegaValor =  pesquisaJogos.value;
 
-    //De para dos nomes do titulo
+//     //De para dos nomes do titulo
 
-    mostraCardJogo.innerHTML = `
-        <div class="card">
-            <div class="card-image ${}"></div>
-            <h2>${}</h2>
-            <p>
-                ${}
-            </p>
-            <a href="#">Comprar</a>
-        </div>
-    `
+//     mostraCardJogo.innerHTML = `
+//         <div class="card">
+//             <div class="card-image ${}"></div>
+//             <h2>${}</h2>
+//             <p>
+//                 ${}
+//             </p>
+//             <a href="#">Comprar</a>
+//         </div>
+//     `
 
-    console.log(pegaValor)
+//     console.log(pegaValor)
+// }
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 1200}px)`;
 }
 
+setInterval(carrossel, 1800);
